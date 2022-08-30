85944f20e9f237907428403ee23d29f2d5ada960
jensenbp <Brian.P.Jensen@saic.com>
2022-08-30 10:11:36 -0500

    Added a bunch of components for reuseability

---
+++ Portfolio/Components/ListGroupItem.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Portfolio.Components
+{
+    partial class ListGroupItem
+    {
+        [Parameter]
+        public List<string> List { get; set; } = new();
+
+        [Parameter]
+        public string Title { get; set; } = string.Empty;
+    }
+}
---
+++ Portfolio/Components/Skills.razor.cs
@@ -0,0 +1,14 @@
+﻿namespace Portfolio.Components
+{
+    partial class Skills
+    {
+        private readonly List<string> PrimarySkills = new() {
+            ".Net", "Unity3D", "JavaScript", "NodeJS", "MongoDB",
+            "Java", "C/C++", "Github", "Windows OS", "HTML", "CSS"
+        };
+
+        private readonly List<string> SecondarySkills = new() {
+            "Forklift", "Electric Pallet Jack", "Scissor Lift", "Order Picker"
+        };
+    }
+}
