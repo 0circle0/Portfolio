e478dc10cef03a4db6f02e3d4719a97ad5eb7208
0circle0 <jensen_305@yahoo.com>
2022-08-30 05:48:23 -0500

    Portfolio looks better created Card Component

---
+++ Portfolio/Components/Card.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Portfolio.Components {
+    partial class Card {
+        [Parameter]
+        public string SrcImage { get; set; } = string.Empty;
+
+        [Parameter]
+        public string Title { get; set; } = string.Empty;
+
+        [Parameter]
+        public string Text {  get; set; } = string.Empty;
+
+        [Parameter]
+        public string Link { get; set; } = string.Empty;
+    }
+}
