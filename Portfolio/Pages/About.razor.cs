dcd041553c1c766e923a2b10699c653103ba9815
0circle0 <jensen_305@yahoo.com>
2022-09-03 10:33:29 -0500

    Minor Site Changes. Added ContactMe page using formsubmit.co

--- Portfolio/Pages/About.razor.cs
+++ Portfolio/Pages/About.razor.cs
@@ -8,19 +8,16 @@
             My first language was QBASIC creating simple programs and giving more lives in Nibbles and Gorillas. 
             My first games were created with Allegro and C/C++ at 16.",
 
-            @"I released my first application August 1 2013 called Sprite Creator 3 using Java Swing. 
+            @"I released my first application August 1 2013 called Sprite Creator 3. 
             It was a free to use and share application that was created using Java Swing. 
             It started as a learning Java project that later was recreated with Unity3D. 
             Sprite Creator 3 has over 300k downloads in 100+ countries as the free version using Java. 
-            The recreated Unity3D version was put on itch.io for purchase.",
+            The recreated Unity3D version was put on itch.io for purchase in 2020.",
 
-            @"Sprite Creator 3 started as a learning project using Java Swing. Later Unity3D and NodeJS Server/Client 
-            games became my passion.",
-
-            @"I started to focus on backend services around 2020 during the pandemic learning Client side prediction, 
-            Server validation, Input Validation, Credential validation, Session Validation, Account creation, 
-            Email varification, Credential validation, State Machines, Rollback, History, Real-Time 
-            Physics were primary goals."
+            @"Later Unity3D and NodeJS Server/Client games became my passion. 
+            I started to focus on backend services around 2020 during the pandemic learning Client side prediction, 
+            Server validation, Input Validation, Credential validation, Session validation, Account creation, 
+            Email varification, Credential validation, State Machines, Rollback and History"
         };
 
         private readonly List<string> Languages = new()
