f1046c1af85782acc9a67ebbb59a029475a1c6bd
0circle0 <jensen_305@yahoo.com>
2022-09-10 10:10:42 -0500

    FloatingInput now requires Text and Id Fixed ContactMe Message box not set to TextArea Changed FloatingInput to not have floating text on message box

---
+++ Portfolio/Components/FloatingInput.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Portfolio.Components
+{
+    partial class FloatingInput
+    {
+        [Parameter]
+        public bool Required { get; set; } = true;
+
+        [Parameter]
+        public bool TextBox { get; set; } = false;
+
+        [Parameter, EditorRequired]
+        public string Text { get; set; } = string.Empty;
+
+        [Parameter, EditorRequired]
+        public string Id { get; set; } = string.Empty;
+    }
+}
